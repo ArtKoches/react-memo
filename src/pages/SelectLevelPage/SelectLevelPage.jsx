3fa9f4a107f449d7bf7a7abcf3851b09a17c940a
Артур Кочесоков <artkoches@gmail.com>
2024-07-12 15:21:09 +0300

    Реализована логика выбора режима сложности и старта игры с кнопки.

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -1,28 +1,65 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./SelectLevelPage.module.css";
+import { Button } from "../../components/Button/Button";
+import { useState } from "react";
 
 export function SelectLevelPage() {
+  const navigate = useNavigate();
+  const [level, setLevel] = useState(null);
+
+  const onChange = event => {
+    const { value } = event.target;
+    setLevel(value);
+  };
+
+  const onGameStart = () => navigate(level);
+
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
         <h1 className={styles.title}>Выбери сложность</h1>
         <ul className={styles.levels}>
           <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/3">
+            <input
+              className={styles.levelSelect}
+              id="1"
+              name="level"
+              type="radio"
+              value="/game/3"
+              onChange={onChange}
+            />
+            <label className={styles.levelLink} htmlFor="1">
               1
-            </Link>
+            </label>
           </li>
           <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/6">
+            <input
+              className={styles.levelSelect}
+              id="2"
+              name="level"
+              type="radio"
+              value="/game/6"
+              onChange={onChange}
+            />
+            <label className={styles.levelLink} htmlFor="2">
               2
-            </Link>
+            </label>
           </li>
           <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/9">
+            <input
+              className={styles.levelSelect}
+              id="3"
+              name="level"
+              type="radio"
+              value="/game/9"
+              onChange={onChange}
+            />
+            <label className={styles.levelLink} htmlFor="3">
               3
-            </Link>
+            </label>
           </li>
         </ul>
+        <Button onClick={onGameStart}>Старт</Button>
       </div>
     </div>
   );
